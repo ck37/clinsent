bb9306b5b7375fd33350ae2ac1af65f1898e2196
Chris Kennedy <chrisken@gmail.com>
2022-05-05 13:15:56 -0400

    Starting with some basics

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# clinsent
+# clinsent - sentiment measurement in clinical notes
+
+## Install
+
+Install the most recent code on GitHub via pip:
+
+```bash
+pip install git+https://github.com/ck37/clinsent/
+```
+
+## Example
+
+## References
---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+from setuptools import setup
+import os
+import sys
+
+_here = os.path.abspath(os.path.dirname(__file__))
+
+version = {}
+with open(os.path.join(_here, 'clinsent', 'version.py')) as f:
+    exec(f.read(), version)
+    
+    
+with open(os.path.join(_here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name = 'clinsent',
+    url = 'https://github.com/ck37/clinsent',
+    author = 'Chris Kennedy, Hunter Mills, Julien Cobert',
+    author_email = 'chrisken@gmail.com',
+    packages = setuptools.find_packages(),
+    install_requires = [],
+    version = version['__version__'],
+    long_description_content_type = "text/markdown",
+    license = 'MIT',
+    description = 'Tools to measure sentiment in clinical notes',
+    long_description = long_description,
+    python_requires = '>=3.6',
+)
