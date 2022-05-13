77e161a02a60553adedf856bdfb983ff359702c4
Chris Kennedy <chrisken@gmail.com>
2022-05-13 13:37:54 -0400

    flesh out __init__.py

--- clinsent/__init__.py
+++ clinsent/__init__.py
@@ -1,1 +1,11 @@
 from .version import __version__
+
+from .sentences import sentence_segment
+from .keywords import KeywordFinder
+
+# if somebody does "from somepackage import *", this is what they will
+# be able to access:
+__all__ = [
+  "KeywordFinder",
+  "sentence_segment"
+]
